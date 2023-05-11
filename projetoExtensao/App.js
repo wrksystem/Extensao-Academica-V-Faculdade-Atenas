fc89e8943d8d8a8091af826802786eac97547ca9
Drikadev <126597972+drika-dev@users.noreply.github.com>
2023-05-10 21:10:55 -0300

    Atualização
    Adicionei Drawer

--- projetoExtensao/App.js
+++ projetoExtensao/App.js
@@ -1,22 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 //import database from "./src/config/firebaseconfig"; apenas retirar do comentário para usar a importação
 
-export default function App() {
+import Cadastro_tarefas from "./src/screens/Cadastro_tarefas";
+import Cadastro_usuario from "./src/screens/Cadastro_usuario";
+import Login from "./src/screens/Login_usuario";
+
+const Drawer = createDrawerNavigator();
+
+function MeuDrawer() {
   return (
-    <View style={styles.container}>
-      <Text>My app Success!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Drawer.Navigator
+      useLegacyImplementation
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: "#e8e8e7",
+          width: 240,
+          primary: "rgb(179, 129, 230)",
+        },
+      }}
+    >
+      <Drawer.Screen name="Login" component={Login} />
+      <Drawer.Screen name="Cadastro Usuario" component={Cadastro_usuario} />
+      <Drawer.Screen name="Cadastro de Tarefas" component={Cadastro_tarefas} />
+    </Drawer.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+  <NavigationContainer >
+    <MeuDrawer />
+  </NavigationContainer >
+  );
+}
